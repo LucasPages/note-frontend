4dbdd7da1455dcfd642264282f1d0129a361ec7e
Lucas Pagès <lucas@fedora.fritz.box>
2024-10-29 13:59:05 +0100

    Added todo comments to map out directions

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
+// TODO: - Add account login --> create login page and change layout based on logging status
+//       - Fetch data user (read data fetch docs Next.js)
+//       - create note interface (after that --> CRUD operations)
+//       - test front end (see nextjs doc)
+
+// TODO: Low priority
+//      - manage fonts (see nextjs docs + intro video) --> two fonts (one for navbar, one for notes)
+//      - add styling options for the user --> changing color mostly, maybe layout format (grid vs list ?)
+
+
 export default function Home() {
   return (
     <></>
